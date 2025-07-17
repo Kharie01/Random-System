bb717688217ae2a981bf16fa22822324f584f6d0
Khoon <kharieladignon01@gmail.com>
2025-07-18 00:59:23 +0800

    Initial commit

---
+++ RandomSystem/src/randomsystem/RandomSystem.java
@@ -0,0 +1,16 @@
+package randomsystem;
+
+/**
+ *
+ * @author Kharie, Emman, Lorenz
+ */
+
+public class RandomSystem {
+
+    public static void main(String[] args) {
+
+        System.out.print("HEHEHEHEHE");
+        
+    }
+    
+}
